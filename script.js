ec1d76c891090d787b0cf8a630690cf9200bac9e
Mateus Lemos <teuse853@gmail.com>
2025-03-04 20:27:00 -0300

    Atualizações

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 
+
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
 })
@@ -15,18 +16,38 @@ overlay.addEventListener('click', ()=>{
 })
 
 
-document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space') {
-        jump();
-    }
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const especialidadesSection = document.querySelector('.especialidades');
+    const projetosSection = document.querySelector('.projetos');
+    const formularioSection = document.querySelector('.formulario');
+    const menuDesktop = document.querySelector('.menu-desktop');
 
-function jump() {
-    const mario = document.querySelector('.mario');
-    if (!mario.classList.contains('jump')) {
-        mario.classList.add('jump');
-        setTimeout(() => {
-            mario.classList.remove('jump');
-        }, 500);
+    function animateOnScroll() {
+        const screenPosition = window.innerHeight;
+
+        // Animação para a seção especialidades
+        const especialidadesPosition = especialidadesSection.getBoundingClientRect().top;
+        if (especialidadesPosition < screenPosition) {
+            especialidadesSection.classList.add('animate');
+        }
+
+        // Animação para a seção projetos
+        const projetosPosition = projetosSection.getBoundingClientRect().top;
+        if (projetosPosition < screenPosition) {
+            projetosSection.classList.add('animate');
+        }
+
+        // Animação para a seção formulario (Fala Comigo)
+        const formularioPosition = formularioSection.getBoundingClientRect().top;
+        if (formularioPosition < screenPosition) {
+            formularioSection.classList.add('animate');
+        }
     }
-}
+
+    function animateMenuOnLoad() {
+        menuDesktop.classList.add('animate');
+    }
+
+    window.addEventListener('scroll', animateOnScroll);
+    animateMenuOnLoad();
+});
